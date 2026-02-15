8dde9d877853c5000bc9e3a64e7adb6a908f94f7
fedenardiii <85617495+fedenardii@users.noreply.github.com>
2026-02-15 02:35:13 +0100

    Create conta_a_capo.s

---
+++ Es3.1/conta_a_capo.s
@@ -0,0 +1,21 @@
+#Scrivere un programma che si comporti come segue:
+#Legge da tastiera una cifra decimale N, controllando che sia valida (cioè un carattere tra ‘0’ e ‘9’).
+#Se N = 0, il programma termina.
+#Altrimenti, va a capo e legge una seconda cifra decimale k, anch’essa con i dovuti controlli.
+#Stampa N righe, dove: la prima riga contiene 1 numero, la seconda riga contiene 2 numeri,…, l’ultima riga contiene N numeri,
+#tutti i numeri stampati formano un’unica sequenza crescente, con passo k, a partire da 1.
+#Stampa una riga vuota e ritorna al punto 1
+
+.include "./files/utility.s"
+.data
+
+.text
+
+main:
+nop
+
+
+
+
+fine:
+ret
