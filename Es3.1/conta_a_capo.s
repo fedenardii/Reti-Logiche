9570d50acd9b30922076bb32eb090aabdb7dda31
fedenardiii <85617495+fedenardii@users.noreply.github.com>
2026-02-15 16:42:49 +0100

    Refactor assembly code for triangle number printing

--- Es3.1/conta_a_capo.s
+++ Es3.1/conta_a_capo.s
@@ -8,14 +8,72 @@
 
 .include "./files/utility.s"
 .data
-
 .text
 
-main:
+_main:
 nop
 
+inizio:
+#1. Inseriamo correttamente il numero N da 0 a 9
+call inserisci_N
 
+#2. Controlliamo che N=0 se non lo è lo mettiamo in SI
+cmp $0, %al
+je fine         #fine del programma
+mov $0, %ah     #estendiamo %ax mettendo %ah per poter fare la mov al registro sorgente 16bit
+mov %ax, %si    #si contiene N numero di righe da stampare
 
+#3. Inseriamo correttamente numero K da 1 a 9
+call newline
+call inserisci_N
+mov $0, %ah     #estendiamo %ax mettendo %ah per poter fare la mov al registro destinatario 16bit
+mov %ax, %di    #di contiene k, che sarà il passo
 
+#4. Inizializzo i registri usati per il triangolo
+mov $1, %ax     #prossimo numero da stampare
+mov $0, %bx     #numeri per riga
+
+#5. Aggiorniamo la riga
+nuovariga:
+call newline
+dec %si
+js rigabianca #se SI<0 allora abbiamo finito tutto, stampiamo la riga bianca sotto
+inc %bx       #infatti riga 1 bx=1, riga 2 bx=2
+mov $0, %dx   #azzeriamo contatore dei numeri sulla riga 
+jmp stampa
+
+#6. Stampiamo la riga bianca
+rigabianca:
+call newline
+jmp inizio 
+
+#7. Stampiamo i numeri del triangolo
+stampa:
+call outdecimal_word
+push %ax
+mov $' ', %al #spazio tra un numero ed un altro
+call outchar 
+pop %ax
+
+add %di, %ax #ax = ax+k
+inc %dx
+cmp %bx, %dx
+je nuovariga
+jmp stampa
+
+#8. Fine programma
 fine:
 ret
+
+
+#funzione inserimento carattere del numero da 0 a 9
+inserisci_N:
+call inchar
+cmp $'0', %al
+jb inserisci_N
+cmp $'9', %al
+ja inserisci_N
+call outchar
+and $0x0F, %al #altro metodo di conversione da carattere char a numero
+ret
+
